76f381f5e7f9abd7490d69c54e79502ffc299a0e
Katiapater <katiasafronova08@yandex.ru>
2022-12-03 15:12:04 +0300

    working program

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Hello World')
+import sys
+import sqlite3
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel, QLineEdit, QComboBox, QTableWidget
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('coffee.ui', self)
+
+        self.db = QSqlDatabase.addDatabase('QSQLITE')
+        self.db.setDatabaseName('coffeeee.sqlite3')
+        self.db.open()
+
+        self.model = QSqlTableModel(self, self.db)
+        self.model.setTable('coffee')
+        self.model.select()
+        self.tableView.setModel(self.model)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
